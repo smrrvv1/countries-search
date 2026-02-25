c5df00ea0689825d6388da5cb85044aa533e6dbf
smrrvv1 <samira.b@compasscollege.art>
2026-02-26 02:14:08 +0600

    add Sidebar list and click logic

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,8 +1,29 @@
 import Drawer from '@mui/material/Drawer';
+import List from '@mui/material/List';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemText from '@mui/material/ListItemText';
+import { ICountryShort } from '../../types';
 
-export const Sidebar = ({countries}:Props) => {
+interface Props {
+    countries: ICountryShort[];
+    onSelect: (code: string) => void;
+  }
+
+export const Sidebar = ({countries, onSelect}:Props) => {
     return (
-    <Drawer open>
-        Sidebar for countries
+    <Drawer open
+        variant="permanent" 
+        anchor="left" 
+        PaperProps={{sx:{ width: 250}}}
+    >
+        <List>
+        {countries.map((country) => (
+            <ListItemButton key={country.alpha3Code} onClick={() => onSelect(country.alpha3Code)}>
+            <ListItemText primary={country.name}/>
+            </ListItemButton>
+        ))
+        }
+      </List>
     </Drawer>
-)}
+)
+}
