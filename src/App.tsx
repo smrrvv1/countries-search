fda31a9c6cd5c8f52a77946e1e3dc48e2a225c62
smrrvv1 <samira.b@compasscollege.art>
2026-03-17 02:36:03 +0600

    remake App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,84 +1,51 @@
-import { useState, useEffect } from "react"
-import { Sidebar } from "./components/Sidebar/Sidebar"
-import { BASE_URL } from "./constants"
-import type { ICountryShort, ICountry } from "./types"
-import Box from '@mui/material/Box';
-import Typography from '@mui/material/Typography';
+import { useState, useEffect } from 'react'
+import { Sidebar } from './components/Sidebar/Sidebar'
+import { CountryInfo } from './components/CountryInfo/CountryInfo'
+import  { axiosApi } from './axiosApi'
+import { ICountryShort, ICountry } from './types'
 
 function App() {
-  const [countriesList, setcountriesList] = useState<ICountryShort[]>([])
+  const [countries, setCountries] = useState<ICountryShort[]>([])
   const [selectedCountry, setSelectedCountry] = useState<ICountry | null>(null)
+  const [borderNames, setBorderNames] = useState<string[]>([])
 
-  useEffect(()=>{
-    const getCountries = async() => {
-      try{
-        const response = await fetch(`${BASE_URL}/all?fields=alpha3Code,name`)
-        if (!response.ok) {
-          throw new Error
-        }
-        const data:ICountryShort[] = await response.json()
-        setcountriesList(data)
-      }catch(e) {
-        console.log(e)
+  useEffect(() => {
+    const getCountries = async () => {
+      try {
+        const response = await axiosApi.get<ICountryShort[]>('/all?fields=alpha3Code,name')
+        setCountries(response.data)
+      } catch (e) {
+        console.error(e)
       }
     }
     getCountries()
-  },[])
+  }, [])
 
-  const getCountryByCode = async (code:string) => {
+  const selectCountry = async (code: string) => {
     try {
-      const response = await fetch(`${BASE_URL}/alpha/${code}`)
-      if (!response.ok) {
-        throw new Error
+      const {data} = await axiosApi.get<ICountry>(`/alpha/${code}`)
+      
+      const names: string[] = []
+      if (data.borders && data.borders.length > 0) {
+        for (const border of data.borders) {
+          const { data: dataBorders } = await axiosApi.get<ICountry>(`/alpha/${border}`)
+          names.push(dataBorders.name)
+        }
       }
-      const data: ICountry = await response.json()
+      
       setSelectedCountry(data)
+      setBorderNames(names)
     } catch (e) {
-      console.log(e)
+      console.error(e)
     }
-  }  
+  }
 
   return (
-    <Box sx={{ display: 'flex' }}>
-      <Sidebar countries={countriesList} onSelect={getCountryByCode}/>
-
-      <Box component="main" sx={{ flexGrow: 1, p: 3, ml: '250px' }}>
-        {selectedCountry ? (
-          <Box>
-            <Typography variant="h4" gutterBottom>{selectedCountry.name}</Typography>
-            <img src={selectedCountry!.flag} alt="flag" style={{ width: '150px', borderRadius: '4px' }} />
-            <Typography variant="body1" sx={{ mt: 2 }}>
-              столица: {selectedCountry.capital}
-            </Typography>
-            <Typography variant="body1">
-                население: {selectedCountry.population.toLocaleString()}
-            </Typography>
-
-            <Typography 
-              variant="h6" sx={{mt: 3}}>
-                границы:
-            </Typography>
-
-            {selectedCountry.borders ? (
-              <Box component="ul">
-                {selectedCountry.borders.map(border => (
-                  <Typography component="li" key={border}>
-                    {border}
-                  </Typography>
-                ))}
-              </Box>
-            ) : (
-              <Typography>нет сухопутных границ</Typography>
-            )}
-          </Box>
-        ) : (
-          <Typography variant="h5">
-            выберите страну в списке слева
-          </Typography>
-        )}
-      </Box>
-    </Box>
-  )         
+    <div style={{display: 'flex'}}>
+      <Sidebar countries={countries} onSelect={selectCountry} />
+      <CountryInfo country={selectedCountry} borders={borderNames} />
+    </div>
+  )
 }
 
 export default App
